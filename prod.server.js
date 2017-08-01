59be934df082f663f5bce0426fd17ed5d0b551e6
ouzhixian <ouzhixian@dandanlicai.com>
2017-08-01 17:42:15 +0800

    update

--- prod.server.js
+++ prod.server.js
@@ -26,21 +26,21 @@ apiRoutes.get('/seller', function (req, res) {
     errno: 0,
     data: seller
   });
-})
+});
 
 apiRoutes.get('/goods', function (req, res) {
   res.json({
     errno: 0,
     data: goods
   });
-})
+});
 
 apiRoutes.get('/ratings', function (req, res) {
   res.json({
     errno: 0,
     data: ratings
   });
-})
+});
 
 app.use('/api', apiRoutes);
 
